7a4e5b5a108011cf50d95161c898dea939243b3d
Shane Sutro <shanesutro@gmail.com>
2020-12-02 21:37:24 -0700

    Complete creating reusable API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,25 @@
+
 var searchYouTube = (options, callback) => {
-  // TODO
+  options.max === undefined ? 5 : options.max;
+  // ajax is sending a requeest to the URL using the GEET method
+  // data: is a object which are our parameters that we are asking for
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {q: options.query, part: 'snippet', maxResults: options.max, videoEmbeddable: true, type: 'video', key: options.key},
+    contentType: 'application/json',
+    // callback is our data were getting back from this AJAX request
+    // success calls a function that has our data inside. so we intercept that data and change it to pass back into our CallBack.
+    success: (data) => {
+      callback(data.items);
+    },
+    error: function(error) {
+      console.error('failed to lookup youtube API', error);
+    }
+  });
+
 };
+
+
 
 export default searchYouTube;
