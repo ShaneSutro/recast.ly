285b5f05cdf0244c7225ff0db5dd3e1380cd06e7
Shane Sutro <shanesutro@gmail.com>
2020-12-02 19:36:16 -0700

    Complete creating a dynamic Video List component

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -26,12 +26,9 @@ import VideoListEntry from './VideoListEntry.js';
 
 var VideoList = (props) => (
   <div className="video-list">
-    <VideoListEntry videos={props.videos[0]}/>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map((video) => (
+      <VideoListEntry video={video}/>
+    ))}
   </div>
 );
 
